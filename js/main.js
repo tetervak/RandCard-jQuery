de113382900615d720d0272ac1baf134d856c364
Alex Tetervak <tetervak@sheridancollege.ca>
2019-09-21 19:47:40 -0400

    switched to using classes

--- js/main.js
+++ js/main.js
@@ -2,52 +2,72 @@
 
 "use strict"; // use the strict mode
 
+class RandomCard{
+
+    // these arrays are used to generate the random card names
+    static SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"];
+    static FACES = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+        "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
+
+    // generate a random card info
+    constructor(){
+        // the random number  0..51
+        let rand = Math.floor(52 * Math.random());
+        // the card number 1..52 is matching the image file name
+        let cardNumber = rand + 1;
+        // the name of the card determined via the math trick
+        this.cardName
+            = RandomCard.FACES[rand % 13]
+            + " of "
+            + RandomCard.SUITS[Math.floor(rand / 13)];
+        // generate the image path
+        this.imgSrc = `images/cards/${cardNumber}.png`;
+    }
+}
+
+class RandomCardApplication{
+
+    constructor(){
+        // generate random card info
+        this.card = new RandomCard();
+        // preload a first random card image file
+        this.preloadCardImage();
+    }
+
+    // preload the image file from the server
+    preloadCardImage(){
+        this.image = new Image();
+        this.image.src = this.card.imgSrc;
+    }
+
+    // called when the document is ready
+    init(){
+        this.showCard();
+        this.setupNextCardLink();
+    }
+
+    // show the card name and the card image on the page
+    showCard(){
+        // insert the card name
+        $("span.cardName").text(this.card.cardName);
+        // insert the image, set the src attribute of the image tag
+        $("#card").attr("src", this.card.imgSrc);
+    }
+
+    // makes "Next Card" link work correctly
+    setupNextCardLink(){
+        $("a:contains('Next Card')").click(event=>{
+            event.preventDefault();
+            this.card = new RandomCard();
+            this.showCard();
+            this.preloadCardImage();
+        });
+    }
+}
+
+let randomCardApplication = new RandomCardApplication();
 $(function(){
-    preloadCard(); // preload a first random card
-    showCard(); // show the first card
-    preloadCard(); // preload a next random card
-    setupNextCardLink(); // make the "Next Card" link work correctly
+    randomCardApplication.init();
 });
 
-// define the Card constructor function
-function Card() {
-    // these arrays are used to generate the random card names
-    const SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"];
-    const FACES = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
-        "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
-    // the random number  0..51
-    let rand = Math.floor(52 * Math.random());
-    // the card number 1..52 is matching the image file name
-    let cardNumber = rand + 1;
-    // the name of the card determined via the math trick
-    this.cardName = FACES[rand % 13] + " of " + SUITS[Math.floor(rand / 13)];
-    // load the image from the server
-    this.imgSrc = "images/cards/" + cardNumber + ".png";
-    this.cardImg = new Image();
-    this.cardImg.src = this.imgSrc;
-}
 
-// to store the pre-loaded card info
-let card;
-
-// generates the random card info and pre-loads the matching card image
-function preloadCard(){
-    card = new Card();
-}
-
-// shows the pre-loaded card name and image
-function showCard(){
-    // insert the card name
-    $("span.cardName").text(card.cardName);
-    // insert the image, set the src attribute of the image tag
-    $("#card").attr("src", card.imgSrc);
-}
-
-// makes the "Next Card" link work correctly
-function setupNextCardLink(){
-    $("a:contains('Next Card')").click(function(event){
-        event.preventDefault();
-        showCard();
-        preloadCard();
-    });
-}
