d0eebe3bae41596b7ad804f9af16a6f09f33ed0a
Alex Tetervak <tetervak@sheridancollege.ca>
2019-09-21 16:06:29 -0400

    using the card object

--- js/main.js
+++ js/main.js
@@ -9,34 +9,38 @@ $(function(){
     setupNextCardLink(); // make the "Next Card" link work correctly
 });
 
-// these arrays are used to generate the random card names
-const SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"];
-const FACES = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
-    "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
-
-// to store the pre-loaded card info
-let cardName, cardNumber, imgSrc, cardImg;
-
-// generates the random card info and pre-loads the matching card image
-function preloadCard(){
+// define the Card constructor function
+function Card() {
+    // these arrays are used to generate the random card names
+    const SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"];
+    const FACES = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+        "Eight", "Nine", "Ten", "Jack", "Queen", "King"];
     // the random number  0..51
     let rand = Math.floor(52 * Math.random());
     // the card number 1..52 is matching the image file name
-    cardNumber = rand + 1;
+    let cardNumber = rand + 1;
     // the name of the card determined via the math trick
-    cardName = FACES[rand % 13] + " of " + SUITS[Math.floor(rand / 13)];
+    this.cardName = FACES[rand % 13] + " of " + SUITS[Math.floor(rand / 13)];
     // load the image from the server
-    imgSrc = `images/cards/${cardNumber}.png`;
-    cardImg = new Image();
-    cardImg.src = imgSrc;
+    this.imgSrc = "images/cards/" + cardNumber + ".png";
+    this.cardImg = new Image();
+    this.cardImg.src = this.imgSrc;
+}
+
+// to store the pre-loaded card info
+let card;
+
+// generates the random card info and pre-loads the matching card image
+function preloadCard(){
+    card = new Card();
 }
 
 // shows the pre-loaded card name and image
 function showCard(){
     // insert the card name
-    $("span.cardName").text(cardName);
+    $("span.cardName").text(card.cardName);
     // insert the image, set the src attribute of the image tag
-    $("#card").attr("src", imgSrc);
+    $("#card").attr("src", card.imgSrc);
 }
 
 // makes the "Next Card" link work correctly
